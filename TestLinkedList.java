ccf95e63d1c75b0c24dc1191ac397302d585cdb9
Natalia Protsko <nprotsko2016@gmail.com>
2017-05-03 23:28:06 +0300

    add through itrator and collection

---
+++ TestLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * Created by nata on 03.05.2017.
+ */
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class TestLinkedList {
+
+    public static void main(String[] args) {
+
+        LinkedList passengerList = new LinkedList();
+
+        passengerList.add("Alex Smith");
+        passengerList.add("Mary Lou");
+        passengerList.add("Sim Monk");
+
+        // Get the list iterator and print every element of the list
+        ListIterator iterator = passengerList.listIterator();
+
+        System.out.println(iterator.next());
+        //1--
+        //iterator.add("VIP customer");
+        //iterator.previous();
+
+        //2--
+        passengerList.add("VIP customer");
+        iterator = passengerList.listIterator();
+
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+    }
+}
